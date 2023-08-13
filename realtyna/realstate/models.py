cbad277aeefdc38c1396721e2ab44f55a308c982
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-08-13 22:31:10 +0330

    add real state models

--- realtyna/realstate/models.py
+++ realtyna/realstate/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from realtyna.utils.models import BaseModel
+
+
+class ListingOwner(BaseModel):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class Room(BaseModel):
+    owned_by = models.ForeignKey(
+        ListingOwner,
+        on_delete=models.CASCADE,
+    )
+
+
+class Reservation(BaseModel):
+    room = models.ForeignKey(
+        Room,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    booked_by = models.CharField(
+        max_length=256,
+    )
+    
+    reserved_until = models.DateTimeField()
