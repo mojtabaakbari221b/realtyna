98d95d8cf0bd217f010d3358ea594272ffd097db
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-08-14 23:51:29 +0330

    add realstate app to root url path

--- configs/urls.py
+++ configs/urls.py
@@ -36,6 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/realstate/', include('realtyna.realstate.urls')),
     path('api/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
