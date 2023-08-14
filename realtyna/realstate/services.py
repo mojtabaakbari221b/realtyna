98d54fa06e35aa23aa74f8759006e52deda0160d
mojtabaakbari <mojtaba.akbari.221b@gmail.com>
2023-08-14 23:52:34 +0330

    room reservation creator service

---
+++ realtyna/realstate/services.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from .models import Reservation, Room
+
+
+def room_reservation_create(*, room: Room, booked_by: str, reserved_until: datetime) -> Reservation:
+    return Reservation.objects.create(
+        room=room,
+        booked_by=booked_by,
+        reserved_until=reserved_until,
+    )
